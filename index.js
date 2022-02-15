bf939d0b5fbb787103307f55be87bd0e24ec722d
SebastianCarrington <64775211+SebastianCarrington@users.noreply.github.com>
2022-02-15 14:44:15 +0000

    Added database storage and other fancy features

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 */
 
 const inquirer = require("inquirer");
+const { createSpinner } = require("nanospinner");
 const chalk = require("chalk");
 const mongoose = require("mongoose");
+
+const volunteer = require("./schemas/volunteer");
 
 // Sets up the .env file
 require("dotenv").config();
@@ -14,19 +17,56 @@ async function main() {
 	console.log(chalk.magentaBright("--- Friends of Seaview Pier Member Registration ---"));
 
 	const answers1 = await inquirer.prompt([
-		{ type: "input", message: "Please enter your first name:", name: "firstname" },
-		{ type: "input", message: "Please enter your last name:", name: "surname" },
-		{ type: "confirm", message: "Do you wish to work as a volunteer?", name: "volunteer" },
+		{
+			type: "input",
+			message: "Please enter your forename:",
+			name: "forename",
+		},
+		{
+			type: "input",
+			message: "Please enter your surname:",
+			name: "surname",
+		},
+		{
+			type: "confirm",
+			message: "Do you wish to work as a volunteer?",
+			name: "volunteer",
+		},
 	]);
 	if (answers1.volunteer) {
-		const answers2 = await inquirer.prompt([
-			{ type: "rawlist", message: "Please select the area you wish to work in.", name: "area", choices: ["Pier entrance gate", "Gift shop", "Painting and Decorating"] },
+		var answers2 = await inquirer.prompt([
+			{
+				type: "rawlist",
+				message: "Please select the area you wish to work in.",
+				name: "area",
+				choices: ["Pier entrance gate", "Gift shop", "Painting and Decorating"],
+			},
 		]);
 	}
 	const answers3 = await inquirer.prompt([
-		{ type: "input", message: "Please enter the date of joining:", name: "date" },
-		{ type: "confirm", message: "Have you paid the $75 membership fee?", name: "paid" },
+		{
+			type: "input",
+			message: "Please enter the date of joining:",
+			name: "date",
+		},
+		{
+			type: "confirm",
+			message: "Have you paid the $75 membership fee?",
+			name: "paid",
+		},
 	]);
+
+	const spinner = createSpinner("Adding to database...").start();
+	await new volunteer({
+		forename: answers1.forename,
+		surname: answers1.surname,
+		isVolunteer: answers1.volunteer,
+		area: answers1.volunteer ? answers2.area : undefined,
+		date: answers3.date,
+		paid: answers3.paid,
+	}).save();
+	spinner.success({ text: `Successfully added user ${answers1.forename} ${answers1.surname} to database.` });
+	process.exit(0);
 }
 
 // Connects to the database, and then runs the main function
